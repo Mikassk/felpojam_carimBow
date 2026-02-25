84f53bcb25e0ff8ab1f7d6f0bffe6321eae8bc2d
Monica Mika Sassaki <MonicaSassaki@FRANCISCANOS.NET>
2026-02-25 10:10:09 -0300

    tela de intro e credito

---
+++ felpojam-2026/script/Intro/intro.gd
@@ -0,0 +1,38 @@
+extends Node2D
+@export var spr: Sprite2D
+@export var camera: Camera2D
+var pos: int = 1
+
+var p_x: Array = [327,-299,327,-346,347,-332]
+var p_y: Array = [1259,1252,189,196,-847,-847]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spr.position.x = p_x[0]
+	spr.position.y = p_y[0]
+	await get_tree().create_timer(2.0).timeout
+	_move_image()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _move_image():
+	var x_ = p_x[pos]
+	var y_ = p_y[pos]
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.tween_property(spr,"position:x",x_,0.6)
+	tween.parallel().tween_property(spr,"position:y",y_,0.6)
+	pos += 1
+	await get_tree().create_timer(2.0).timeout
+	
+	if pos < p_x.size():
+		_move_image()
+	
+	
+	
+	
